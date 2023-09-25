a1bf87f127da2e811f0038c2f4faf3a8bd0fa2cc
BraianSalavarria <braian.salavarria@gmail.com>
2023-09-25 20:06:58 -0300

    se corrigio el faltante del ejercicio 2

--- ingresarAlumnos.js
+++ ingresarAlumnos.js
@@ -1,3 +1,59 @@
+//ejercicio 2:
+
+//Definimos el objeto persona
+
+function Persona(nombre, apellido, genero){
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.genero = genero;
+}
+
+Persona.prototype.obtDetalles = function() {
+  console.log("nombre: "+ this.nombre +" Apellido: "+ this.apellido +" genero: "+ this.genero);
+};
+
+function Estudiante (nombre, apellido, genero, curso, grupo) {
+  Persona.call(this, nombre, apellido, genero);
+  this.curso = curso;
+  this.grupo = grupo;
+}
+
+//realizamos una herencaia  
+Estudiante.prototype = Object.create(Persona.prototype);
+
+//metodo registrar de Estudiante
+Estudiante.prototype.registrar = function(){
+  console.log(this.nombre+" "+this.apellido+ " se ha registrado en el grupo: " +this.grupo);
+};
+
+function Profesor(nombre, apellido, genero, asignatura, comicion){
+  Persona.call(this , nombre, apellido, genero);
+  this.asignatura = asignatura;
+  this.comicion = comicion;
+}
+// realizamos una herencia
+Profesor.prototype= Object.create(Persona.prototype);
+
+//metodo para asignar profesor
+Profesor.prototype.asignar = function(){
+  console.log("El profesor: "+this.nombre +" "+ this.apellido +" Se registro en la comision: "+this.comicion);
+}
+
+
+//pruebas
+console.log("ejecutando pruebas: ");
+
+let estudiante = new Estudiante("BRAIAN","SALAVARRIA","MASCULINO","PROGRAMACION 2","GRUPO 2");
+let profesor = new Profesor("CARLOS","SANTILLAN","MASCULINO","ANALISIS MATEMATICO","GRUPO 2");
+
+estudiante.obtDetalles();
+estudiante.registrar();
+
+profesor.obtDetalles();
+profesor.asignar();
+
+//ejercicio 3
+
 const nombre = document.getElementById('nombre')
 const apellido = document.getElementById('apellido')
 const correo = document.getElementById('email')
